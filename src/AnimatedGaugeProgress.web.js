95aac96e43b1b241fce7d9d9843cc98e61f0631f
Joseph <joeclough@hotmail.co.uk>
2019-05-02 17:45:49 +0100

    Added animation for web support 0.1

---
+++ src/AnimatedGaugeProgress.web.js
@@ -0,0 +1,159 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class AnimatedGaugeProgress extends React.Component {
+	state = {
+		chartFillAnimation: new Animated.Value(this.props.prefill || 0)
+	};
+
+	componentDidMount() {
+		this.animateFill();
+		AppState.addEventListener('change', this._handleAppStateChange);
+	}
+
+	componentWillUnmount() {
+		AppState.removeEventListener('change', this._handleAppStateChange);
+	}
+
+	_handleAppStateChange = nextAppState => {
+		if (nextAppState === ActiveState) this.animateFill();
+	};
+
+	componentDidUpdate(prevProps) {
+		if (prevProps.fill !== this.props.fill) {
+			this.animateFill();
+		}
+	}
+
+	polarToCartesian = (centerX, centerY, radius, angleInDegrees) => {
+		var angleInRadians = ((angleInDegrees - 180) * Math.PI) / 180;
+
+		return {
+			x: centerX + radius * Math.cos(angleInRadians),
+			y: centerY + radius * Math.sin(angleInRadians)
+		};
+	};
+
+	describeArc = (x, y, radius, startAngle, endAngle) => {
+		var start = this.polarToCartesian(x, y, radius, endAngle);
+		var end = this.polarToCartesian(x, y, radius, startAngle);
+
+		var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+
+		var d = [
+			'M',
+			start.x,
+			start.y,
+			'A',
+			radius,
+			radius,
+			0,
+			largeArcFlag,
+			0,
+			end.x,
+			end.y
+		].join(' ');
+
+		console.log(d);
+		return d;
+	};
+
+	backgroundArc = (x, y, radius, startAngle, endAngle) => {
+		const d = this.describeArc(x, y, radius, startAngle, endAngle);
+		console.log(d);
+		return d;
+	};
+
+	foregroundArc = (x, y, radius, startAngle, endAngle) => {
+		const d = this.describeArc(x, y, radius, startAngle, endAngle);
+		return d;
+	};
+
+	render() {
+		const namespacedSVG = `<animateMotion fill="freeze" dur="${
+			this.props.speed
+		}s" repeatCount="forwards" keyPoints="1;0" keyTimes="0;1" rotate="auto" calcMode="linear"><mpath xlink:href="#arc2"/></animateMotion>`;
+		return (
+			<svg
+				width={this.props.size + this.props.offset + 5}
+				height={this.props.size + this.props.offset + 5}
+			>
+				<path
+					id="arc1"
+					fill="none"
+					stroke={this.props.backgroundColor}
+					strokeWidth={this.props.width}
+					strokeCap={this.props.strokeCap}
+					d={this.backgroundArc(
+						this.props.size / 2,
+						this.props.size / 2,
+						this.props.size / 2 - this.props.width / 2,
+						0,
+						(360 * 99.9) / 100 - this.props.cropDegree
+					)}
+				/>
+				<path
+					className="path"
+					id="arc2"
+					fill="none"
+					stroke={this.props.tintColor}
+					strokeCap={this.props.strokeCap}
+					strokeMiterlimit={10}
+					strokeWidth={this.props.width}
+					d={this.foregroundArc(
+						this.props.size / 2,
+						this.props.size / 2,
+						this.props.size / 2 - this.props.width / 2,
+						0,
+						(((360 * 99.9) / 100 - this.props.cropDegree) *
+							this.props.fill) /
+							100
+					)}
+				/>
+				<circle
+					width={10}
+					height={10}
+					r={this.props.capWidth / 2}
+					stroke={this.props.capColor}
+					strokeWidth={this.props.capWidth}
+					dangerouslySetInnerHTML={{ __html: namespacedSVG }}
+				/>
+			</svg>
+		);
+	}
+}
+
+AnimatedGaugeProgress.propTypes = {
+	style: ViewPropTypes.style,
+	size: PropTypes.number.isRequired,
+	fill: PropTypes.number,
+	prefill: PropTypes.number,
+	width: PropTypes.number.isRequired,
+	tintColor: PropTypes.string,
+	backgroundColor: PropTypes.string,
+	stroke: PropTypes.arrayOf(PropTypes.number),
+	strokeCap: PropTypes.string,
+	backgroundColor: PropTypes.string,
+	rotation: PropTypes.number,
+	offset: PropTypes.number,
+	cropDegree: PropTypes.number,
+	speed: PropTypes.number,
+	capColor: PropTypes.string,
+	capWidth: PropTypes.number,
+	circleRadian: PropTypes.number,
+	onAnimationComplete: PropTypes.func,
+	onLinearAnimationComplete: PropTypes.func
+};
+
+AnimatedGaugeProgress.defaultProps = {
+	tintColor: 'black',
+	backgroundColor: '#e4e4e4',
+	rotation: 90,
+	cropDegree: 90,
+	strokeCap: 'butt',
+	capColor: 'black',
+	capWidth: 0,
+	circleRadian: 360,
+	offset: 0,
+	speed: 0.6
+};
